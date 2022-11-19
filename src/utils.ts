a29df6c32edf4847988231c427bd5b5c77ea97b2
Germán Castro <german@strivelabs.io>
2022-11-19 19:10:37 +0100

    colored messages

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,14 @@
+import chalk from 'chalk';
+
 export function exit(message?: string) {
-  message ?? console.log(message);
+  message && errorLog(message);
   process.exit(0);
+}
+
+export function errorLog(...messages: string[]) {
+  console.error(chalk.red(messages));
+}
+
+export function successLog(...messages: string[]) {
+  console.log(chalk.green(messages));
 }
