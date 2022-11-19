f3a7f6cfee875a1441a65d6b1694d740e7cc48b7
Germán Castro <german@strivelabs.io>
2022-11-19 19:10:56 +0100

    parallel downloads

--- src/download.ts
+++ src/download.ts
@@ -1,16 +1,19 @@
 import Path from 'path';
 import got from 'got';
 import { createWriteStream } from 'fs';
-import { map, parallel, retry } from 'radash'
+import * as _ from 'radash'
+import { errorLog, successLog } from './utils.js';
 
-
-export const downloadTorrents = async (urls: string[], folder: string) => {
-  return map(urls, (url) => downloadFile(url, folder));
-}
+const PARALLEL_DOWNLOADS = 5;
+export const downloadTorrents = async (urls: string[], folder: string) => _.parallel(PARALLEL_DOWNLOADS, urls, async (url) => {
+  const [err, path] = await _.try(downloadFile)(url, folder);
+  if (err) errorLog('Error downloading file', url);
+  else successLog(`${path} downloaded`);
+  return path;
+});
 
 export const downloadFile = async (url: string, folder: string) => {
     const filename = url.split('/').pop() as string;
-    console.log(`Downloading ${filename}...`);
     const path = Path.normalize(Path.resolve(folder, filename));
     // TODO: mkdir if not exists
 
